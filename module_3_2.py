fa4dbfeed1a13301c84019c64db09b7cf0071ded
RionGate <168349398+RionGate@users.noreply.github.com>
2024-06-28 20:13:18 +0700

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,19 @@
+def send_email(message, recipient,*, sender = "university.help@gmail.com"):
+    recipient_var = '@' in recipient and (recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net'))
+    sender_var = '@' in sender and (sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net'))
+    if recipient_var and sender_var:
+        if recipient == sender:
+            print("Нельзя отправить письмо самому себе!")
+        else:
+            if sender == "university.help@gmail.com":
+                print("Письмо успешно отправлено с адреса", sender,"на адрес",recipient,".")
+            else:
+                print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса",sender,"на адрес", recipient,".")
+    else:
+        print("Невозможно отправить письмо с адреса", sender, "на адрес", recipient)
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
